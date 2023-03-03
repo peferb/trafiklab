2ce10bc3355b1f676418b665fb51aa0c9d3b6064
Per-Erik Ferb <pe.ferb@gmail.com>
2023-03-03 22:11:14 +0100

    Rewrite to just return body as string

--- trafiklab.go
+++ trafiklab.go
@@ -1,19 +1,30 @@
 package trafiklab
 
 import (
-	"log"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
 )
 
-func GetSLRealtimeDeparturesV4(query url.Values) (*http.Response, error) {
+func get(url string) (string, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+	return string(bodyBytes), nil
+}
+
+func GetSLRealtimeDeparturesV4(query url.Values) (string, error) {
 	query.Set("key", os.Getenv("SL_REAL_TIME_DEPARTURES_V4"))
 	u, _ := url.Parse("https://api.sl.se/api2/realtimedeparturesV4.json")
 	u.RawQuery = query.Encode()
-	return http.Get(u.String())
-}
-
-func Hello() {
-	log.Print("Hello World")
+	return get(u.String())
 }
--- trafiklab_test.go
+++ trafiklab_test.go
@@ -2,16 +2,20 @@ package trafiklab
 
 import (
 	"net/url"
+	"strings"
 	"testing"
 )
 
-// TestGetSLRealtimeDeparturesV4 calls trafiklab.TestGetSLRealtimeDeparturesV4 siteId 100o and wants status code "200 OK"
+// TestGetSLRealtimeDeparturesV4 calls trafiklab.TestGetSLRealtimeDeparturesV4 siteId 9710 and wants status 0
 func TestGetSLRealtimeDeparturesV4(t *testing.T) {
 	q := url.Values{}
-	q.Set("format", "json")
-	q.Set("Siteid", "1000")
-	res, err := GetSLRealtimeDeparturesV4(q)
-	if res.Status != "200 OK" || err != nil {
-		t.Fatalf(`TestGetSLRealtimeDeparturesV4(siteid=1000) returns "%s" with "%s""`, res.Status, err)
+	q.Set("Siteid", "9710")
+	data, err := GetSLRealtimeDeparturesV4(q)
+
+	if strings.Contains(data, `"StatusCode":0`) {
+		t.Fatalf(`TestGetSLRealtimeDeparturesV4(siteid=1000) data did not contain "StatusCode:0" returned "%s""`, data)
+	}
+	if err != nil {
+		t.Fatalf(`TestGetSLRealtimeDeparturesV4(siteid=1000) threw error "%s""`, err)
 	}
 }
