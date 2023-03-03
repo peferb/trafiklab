152822abdff8a886adaf049edc57b58dbf378210
Per-Erik Ferb <pe.ferb@gmail.com>
2023-03-03 17:20:31 +0100

    Just expose function

--- trafiklab.go
+++ trafiklab.go
@@ -6,9 +6,7 @@ import (
 	"os"
 )
 
-type SL struct{}
-
-func (receiver SL) GetRealtimeDeparturesV4(query url.Values) (*http.Response, error) {
+func GetSLRealtimeDeparturesV4(query url.Values) (*http.Response, error) {
 	query.Set("key", os.Getenv("SL_REAL_TIME_DEPARTURES_V4"))
 	u, _ := url.Parse("https://api.sl.se/api2/realtimedeparturesV4.json")
 	u.RawQuery = query.Encode()
