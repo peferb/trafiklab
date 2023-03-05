14307d66f8524d63228f39e697251c52885bc221
Per-Erik Ferb <pe.ferb@gmail.com>
2023-03-05 16:22:26 +0100

    Added package for "SL Service alerts v2.0" API (except struct getter)

---
+++ sl/service_alerts_v2/service_alerts_v2.go
@@ -0,0 +1,48 @@
+// Package realtime_departures_v4 "SL Service alerts v2.0 AKA Deviations (Störningsinformation 2)"
+//
+// Access
+// https://developer.trafiklab.se/api/sl-storningsinformation-2
+//
+// Documentation
+// https://www.trafiklab.se/api/trafiklab-apis/sl/service-alerts-2/
+package realtime_departures_v4
+
+import (
+	sl "github.com/peferb/trafiklab/sl"
+	"net/url"
+)
+
+const ApiUrlJson = "https://api.sl.se/api2/realtimedeparturesV4.json"
+const ApiUrlXML = "https://api.sl.se/api2/realtimedeparturesV4.xml"
+
+// GetBytes JSON
+func GetBytes(apiKey string, query url.Values) ([]byte, error) {
+	return sl.GetBytes(apiKey, query, ApiUrlJson)
+}
+
+// TODO Struct setter
+// TODO Response struct
+
+func GetJson(apiKey string, query url.Values) (string, error) {
+	bytes, err := sl.GetBytes(
+		apiKey,
+		query,
+		ApiUrlJson,
+	)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func GetXml(apiKey string, query url.Values) (string, error) {
+	bytes, err := sl.GetBytes(
+		apiKey,
+		query,
+		ApiUrlXML,
+	)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
