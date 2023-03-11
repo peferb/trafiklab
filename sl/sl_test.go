f4f355c5e9d5c291e6cf8e92ce2ea544a5b1eaaf
Per-Erik Ferb <pe.ferb@gmail.com>
2023-03-11 18:45:03 +0100

    Test the rest of the API:s

--- sl/sl_test.go
+++ sl/sl_test.go
@@ -79,6 +79,10 @@ func testStatusCode(testName string, t *testing.T, envVar string, api Type, form
 	}
 }
 
+/*
+ * RealTimeDeparturesV4
+ */
+
 // TestRealTimeDeparturesV4StringJson tries to fetch data from RealTimeDeparturesV4 API
 func TestRealTimeDeparturesV4StringJson(t *testing.T) {
 	apiParams := url.Values{"siteid": {"9710"}}
@@ -113,4 +117,124 @@ func TestRealTimeDeparturesV4ResponseJson(t *testing.T) {
 func TestRealTimeDeparturesV4ResponseXml(t *testing.T) {
 	apiParams := url.Values{"siteid": {"9710"}}
 	testStatusCode("TestRealTimeDeparturesV4ResponseXml", t, "SL_REAL_TIME_DEPARTURES_V4", RealTimeDeparturesV4, XML, apiParams, "response", statusSuccessXml)
+}
+
+/*
+ * ServiceAlertsV2
+ */
+
+// TestServiceAlertsV2StringJson tries to fetch data from ServiceAlertsV2 API
+func TestServiceAlertsV2StringJson(t *testing.T) {
+	apiParams := url.Values{"siteid": {"9000"}}
+	testStatusCode("TestServiceAlertsV2StringJson", t, "SL_SERVICE_ALERTS_V2", ServiceAlertsV2, JSON, apiParams, "string", statusSuccessJson)
+}
+
+// TestServiceAlertsV2StringXml tries to fetch data from ServiceAlertsV2 API
+func TestServiceAlertsV2StringXml(t *testing.T) {
+	apiParams := url.Values{"siteid": {"9000"}}
+	testStatusCode("TestServiceAlertsV2StringXml", t, "SL_SERVICE_ALERTS_V2", ServiceAlertsV2, XML, apiParams, "string", statusSuccessXml)
+}
+
+// TestServiceAlertsV2BytesJson tries to fetch data from ServiceAlertsV2 API
+func TestServiceAlertsV2BytesJson(t *testing.T) {
+	apiParams := url.Values{"siteid": {"9000"}}
+	testStatusCode("TestServiceAlertsV2BytesJson", t, "SL_SERVICE_ALERTS_V2", ServiceAlertsV2, JSON, apiParams, "bytes", statusSuccessJson)
+}
+
+// TestServiceAlertsV2BytesXml tries to fetch data from ServiceAlertsV2 API
+func TestServiceAlertsV2BytesXml(t *testing.T) {
+	apiParams := url.Values{"siteid": {"9000"}}
+	testStatusCode("TestServiceAlertsV2BytesXml", t, "SL_SERVICE_ALERTS_V2", ServiceAlertsV2, XML, apiParams, "bytes", statusSuccessXml)
+}
+
+// TestServiceAlertsV2ResponseJson tries to fetch data from ServiceAlertsV2 API
+func TestServiceAlertsV2ResponseJson(t *testing.T) {
+	apiParams := url.Values{"siteid": {"9000"}}
+	testStatusCode("TestServiceAlertsV2ResponseJson", t, "SL_SERVICE_ALERTS_V2", ServiceAlertsV2, JSON, apiParams, "response", statusSuccessJson)
+}
+
+// TestServiceAlertsV2ResponseXml tries to fetch data from ServiceAlertsV2 API
+func TestServiceAlertsV2ResponseXml(t *testing.T) {
+	apiParams := url.Values{"siteid": {"9000"}}
+	testStatusCode("TestServiceAlertsV2ResponseXml", t, "SL_SERVICE_ALERTS_V2", ServiceAlertsV2, XML, apiParams, "response", statusSuccessXml)
+}
+
+/*
+ * StopLookupV1
+ */
+
+// TestStopLookupV1StringJson tries to fetch data from StopLookupV1 API
+func TestStopLookupV1StringJson(t *testing.T) {
+	apiParams := url.Values{"searchstring": {"stockholm"}}
+	testStatusCode("StopLookupV1StringJson", t, "SL_STOP_LOOKUP_V1", StopLookupV1, JSON, apiParams, "string", statusSuccessJson)
+}
+
+// TestStopLookupV1StringXml tries to fetch data from StopLookupV1 API
+func TestStopLookupV1StringXml(t *testing.T) {
+	apiParams := url.Values{"searchstring": {"stockholm"}}
+	testStatusCode("StopLookupV1StringXml", t, "SL_STOP_LOOKUP_V1", StopLookupV1, XML, apiParams, "string", statusSuccessXml)
+}
+
+// TestStopLookupV1BytesJson tries to fetch data from StopLookupV1 API
+func TestStopLookupV1BytesJson(t *testing.T) {
+	apiParams := url.Values{"searchstring": {"stockholm"}}
+	testStatusCode("StopLookupV1BytesJson", t, "SL_STOP_LOOKUP_V1", StopLookupV1, JSON, apiParams, "bytes", statusSuccessJson)
+}
+
+// TestStopLookupV1BytesXml tries to fetch data from StopLookupV1 API
+func TestStopLookupV1BytesXml(t *testing.T) {
+	apiParams := url.Values{"searchstring": {"stockholm"}}
+	testStatusCode("StopLookupV1BytesXml", t, "SL_STOP_LOOKUP_V1", StopLookupV1, XML, apiParams, "bytes", statusSuccessXml)
+}
+
+// TestStopLookupV1ResponseJson tries to fetch data from StopLookupV1 API
+func TestStopLookupV1ResponseJson(t *testing.T) {
+	apiParams := url.Values{"searchstring": {"stockholm"}}
+	testStatusCode("StopLookupV1ResponseJson", t, "SL_STOP_LOOKUP_V1", StopLookupV1, JSON, apiParams, "response", statusSuccessJson)
+}
+
+// TestStopLookupV1ResponseXml tries to fetch data from StopLookupV1 API
+func TestStopLookupV1ResponseXml(t *testing.T) {
+	apiParams := url.Values{"searchstring": {"stockholm"}}
+	testStatusCode("StopLookupV1ResponseXml", t, "SL_STOP_LOOKUP_V1", StopLookupV1, XML, apiParams, "response", statusSuccessXml)
+}
+
+/*
+ * TrafficStatusV2
+ */
+
+// TestTrafficStatusV2StringJson tries to fetch data from TrafficStatusV2 API
+func TestTrafficStatusV2StringJson(t *testing.T) {
+	apiParams := url.Values{}
+	testStatusCode("TrafficStatusV2StringJson", t, "SL_TRAFFIC_STATUS_V2", TrafficStatusV2, JSON, apiParams, "string", statusSuccessJson)
+}
+
+// TestTrafficStatusV2StringXml tries to fetch data from TrafficStatusV2 API
+func TestTrafficStatusV2StringXml(t *testing.T) {
+	apiParams := url.Values{}
+	testStatusCode("TrafficStatusV2StringXml", t, "SL_TRAFFIC_STATUS_V2", TrafficStatusV2, XML, apiParams, "string", statusSuccessXml)
+}
+
+// TestTrafficStatusV2BytesJson tries to fetch data from TrafficStatusV2 API
+func TestTrafficStatusV2BytesJson(t *testing.T) {
+	apiParams := url.Values{}
+	testStatusCode("TrafficStatusV2BytesJson", t, "SL_TRAFFIC_STATUS_V2", TrafficStatusV2, JSON, apiParams, "bytes", statusSuccessJson)
+}
+
+// TestTrafficStatusV2BytesXml tries to fetch data from TrafficStatusV2 API
+func TestTrafficStatusV2BytesXml(t *testing.T) {
+	apiParams := url.Values{}
+	testStatusCode("TrafficStatusV2BytesXml", t, "SL_TRAFFIC_STATUS_V2", TrafficStatusV2, XML, apiParams, "bytes", statusSuccessXml)
+}
+
+// TestTrafficStatusV2ResponseJson tries to fetch data from TrafficStatusV2 API
+func TestTrafficStatusV2ResponseJson(t *testing.T) {
+	apiParams := url.Values{}
+	testStatusCode("TrafficStatusV2ResponseJson", t, "SL_TRAFFIC_STATUS_V2", TrafficStatusV2, JSON, apiParams, "response", statusSuccessJson)
+}
+
+// TestTrafficStatusV2ResponseXml tries to fetch data from TrafficStatusV2 API
+func TestTrafficStatusV2ResponseXml(t *testing.T) {
+	apiParams := url.Values{}
+	testStatusCode("TrafficStatusV2ResponseXml", t, "SL_TRAFFIC_STATUS_V2", TrafficStatusV2, XML, apiParams, "response", statusSuccessXml)
 }
